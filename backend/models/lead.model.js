836d4f3c344cd526f974526c9324933f06dcf75c
clement <clement.lidar@gmail.com>
2023-03-15 13:33:43 +0100

    add isVerified champ to lead model

--- backend/models/lead.model.js
+++ backend/models/lead.model.js
@@ -1,40 +1,38 @@
+const { boolean } = require("joi");
 const mongoose = require("mongoose");
 
 const leadSchema = mongoose.Schema(
   {
     dealerID: {
       type: String,
-      // required: true,
+      required: true,
     },
     first_name: {
       type: String,
-      // required: true,
     },
     last_name: {
       type: String,
-      // required: true,
     },
     email: {
       type: String,
-      // required: true,
     },
     phone_number: {
       type: String,
     },
     company: {
       type: String,
-      // required: true,
+      required: true,
     },
     lookingFor: {
       type: String,
-      // required: true,
+      required: true,
     },
     role: {
       type: String,
-      // required: true,
     },
-    price: {
-      type: Number,
+    isVerified: {
+      type: Boolean,
+      default: false,
     },
     buyer: {
       type: [String],
