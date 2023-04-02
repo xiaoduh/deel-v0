a8fb6f1b702fcd4ddfc3f6c57ba51ed71028e7de
clement <clement.lidar@gmail.com>
2023-04-02 19:22:27 +0200

    const port added

--- backend/server.js
+++ backend/server.js
@@ -48,4 +48,5 @@ app.use("/api/coin", coinRoutes);
 app.use("/api/stripe", stripeRoutes);
 
 // Lancer le server
-app.listen(5000, () => console.log("Server is running au port 5000"));
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log("Server is running au port " + port));
