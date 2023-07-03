4b2f4f9b9ecadd80d2779bae6c78463cc9d14197
clement <clement.lidar@gmail.com>
2023-07-03 11:42:16 +0200

    sameSite:'none'

--- controllers/auth.user.controller.js
+++ controllers/auth.user.controller.js
@@ -197,7 +197,7 @@ module.exports.signInUser = async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: true,
       maxAge: maxAge,
-      SameSite: "None",
+      sameSite: "none",
     });
     res.status(200).json({ user: user._id });
   } catch (err) {
